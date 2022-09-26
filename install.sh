2c417b23ae4a05f043ab8dadfbe6dc694b045536
prakharporwal <rsanshul99@gmail.com>
2022-09-26 10:36:19 +0530

    ubuntu install.sh added

---
+++ install.sh
@@ -0,0 +1,36 @@
+sudo apt update
+
+sudo apt install -y curl i3-wm docker.io docker git
+
+# aws cli2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+
+# brave install
+sudo apt install -y apt-transport-https
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+sudo snap install code --classic
+
+sudo apt install zsh zsh-autosuggestions
+sudo chsh /usr/bin/zsh
+
+#kubernetes minikube
+sudo apt-get install -y ca-certificates
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# upgrade at last
+sudo apt upgrade
+sudo snap install goland --classic
+sudo snap install intellij-idea-community --classic
+
+# remove unecessary
+sudo apt autoremove
